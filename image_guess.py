8dc5685884c92992182691adb2a028d42e20a23c
박찬환 <chanchanhwan@naver.com>
2020-04-13 23:09:30 +0900

    20200413

---
+++ image_guess.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+from datetime import datetime
+import hashlib
+import os.path
+import random
+import re
+import struct
+import sys
+import tarfile
+
+import numpy as np
+from six.moves import urllib
+import tensorflow as tf
+
+from tensorflow.python.framework import graph_util
+from tensorflow.python.framework import tensor_shape
+from tensorflow.python.platform import gfile
+from tensorflow.python.util import compat
